5f806d06866b0353c342bc669db87c94356c3828
pranavyamjal <yamjal0188@gmail.com>
2024-12-19 18:28:06 +0530

    Add user controller login and logout, routes and auth middleware

--- 4. Youtube Backend/src/controllers/user.controller.js
+++ 4. Youtube Backend/src/controllers/user.controller.js
@@ -5,6 +5,29 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 
+// method for generation of access and refresh tokens (JWT)
+
+const generateAccessAndRefreshTokens = async(userId)=> {
+
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken, refreshToken}
+
+
+
+        
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+
+}
+
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
     // validation - not empty
@@ -80,4 +103,88 @@ const registerUser = asyncHandler( async (req, res) => {
 } )
 
 
-export {registerUser} 
+const loginUser = asyncHandler( async (req, res) => {
+    // req body -> data
+    // username or email
+    // find the user
+    // password check
+    // access and refresh tokens
+    // send cookie
+
+
+    const {email, username, password} = req.body
+    if(!username || !email) {
+        throw new ApiError(400, "username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(!user) {
+        throw new ApiError(404, "User does not exist ")
+    }
+
+    // User is a mongodb User object & user is a javascript object (for developing business logic)
+    
+    const isPasswordValid = await user.isPasswordValid(password)
+
+    if( !isPasswordValid){
+        throw new ApiError("401", "Invalid user credentials")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+
+    const loggedInUser = await User.findById(user.id).select("-passsword -refreshToken")
+
+
+    // cookies
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken,
+                refreshToken
+            },
+            "User logged in Successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler( async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            refreshToken: undefined
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        sercure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out"))
+})
+
+export {registerUser,
+        loginUser
+} 
--- 4. Youtube Backend/src/routes/user.routes.js
+++ 4. Youtube Backend/src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import {Router } from "express"
-import { registerUser } from "../controllers/user.controller.js"
+import { loginUser, registerUser } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middlewares.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+
 
 const router = Router()
 
@@ -17,4 +19,9 @@ router.route("/register").post(
     ]),
     registerUser)
 
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 export default router
