10b15bda5f213a492e6ae627024cb4c66840d53d
pranavyamjal <yamjal0188@gmail.com>
2024-12-22 10:29:03 +0530

    Add subscription model and user management endpoints

---
+++ 4. Youtube Backend/src/models/subscription.model.js
@@ -0,0 +1,16 @@
+import mongoose, {Schema} from moongose
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // on who is subscribing
+        ref: "User"
+    },
+    channel : {
+        type: Schema.Types.ObjectId, //one to whom 'subscriber' is subscribing
+        ref:"User"
+    }
+ }, {timestamps: true})
+
+
+ 
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
