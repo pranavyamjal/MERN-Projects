04fa3cca9c9f604e178d11ae6a256cbc47d123a2
pranavyamjal <yamjal0188@gmail.com>
2024-12-24 20:31:33 +0530

    Add new routes for user controllers

--- 4. Youtube Backend/src/routes/user.routes.js
+++ 4. Youtube Backend/src/routes/user.routes.js
@@ -1,7 +1,18 @@
 import {Router } from "express"
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js"
+import { changeCurrentPassword,
+         getCurrentUser, 
+         getUserChannelProfile, 
+         getWatchHistory, 
+         loginUser, 
+         logoutUser, 
+         refreshAccessToken, 
+         registerUser, 
+         updateAccountDetails, 
+         updateUserAvatar, 
+         updateUserCoverAvatar } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
+import multer from "multer"
 
 
 const router = Router()
@@ -26,5 +37,19 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT,logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/update-avatar").patch(verifyJWT, upload.single, updateUserAvatar)
+
+router.route("/update-cover-image").patch(verifyJWT, multer.single,updateUserCoverAvatar)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
